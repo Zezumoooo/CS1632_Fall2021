7d7db376578ba09e0655f8975b8bd4c280942676
Zezumoooo <63968507+Zezumoooo@users.noreply.github.com>
2021-12-17 02:38:40 -0500

    Update GameOfLifePinningTest.java

--- deliverables/4/src/GameOfLifePinningTest.java
+++ deliverables/4/src/GameOfLifePinningTest.java
@@ -14,63 +14,65 @@ public class GameOfLifePinningTest {
 		Cell[][] boardCells=new Cell[5][5];
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j <5; j++) {
-				cell[i][j]=new Cell();
+				Mockito.when(boardCells[i][j].getAlive()).thenReturn(false);
 			}
 		}
+		panel=new MainPanel(boardCells);
 		for(int j=1;j<=3;j++) {
-			cell[2][j].setAlive(true);
+			Mockito.when(boardCells[2][j].getAlive()).thenReturn(true);
 		}
-		panel.setCells(cell);
+		
 }
 @Test 
 	public void test1() {
-	Cell[][] boardCells=new Cell[5][5];
 	panel.calculateNextIteration();
 	for(int i=0;i<2;i++) {
 		for(int j=0;j<5;j++) {
-			assertFalse(cell[j][i].getAlive());
+			assertFalse(boardCells[j][i].getAlive());
 		}
 	}
 	for(int i=1;i<=3;i++) {
-		assertTrue(cell[i][2].getAlive());
+		assertTrue(boardCells[i][2].getAlive());
 	}
 		for(int i=3;i<5;i++) {
 			for(int j=0;j<5;j++) {
-				assertFasle(cell[j][i].getAlive());
+				assertFasle(boardCells[j][i].getAlive());
 			}
 		}
 		panel.calculateNextIteration();
 		for(int i=0;i<2;i++) {
 			for(int j=0;j<5;j++) {
-				assertFasle(cell[j][i].getAlive());
+				assertFalse(boardCells[j][i].getAlive());
 			}
 		}
 		for(int i=1;i<=3;i++) {
-			assertTrue(cell[2][i].getAlive());
+			assertTrue(boardCells[2][i].getAlive());
 		}
 		for(int i=3;i<5;i++) {
 			for(int j=0;j<5;j++) {
-				assertFalse(cell[j][i].getAlive());
+				assertFalse(boardCells[j][i].getAlive());
 			}
 		}
 }
 @Test 
 	public void test2() {
-	Cell[][] boardCells=new Cell[5][5];
-	for (int i = 0; i < 5; i++) 
-	{
-		for (int j = 0; j < 5; j++) 
-		{
-			if (j == 2) {
-				if(i == 1 || i == 2 || i == 3) {
-					assertEquals(boardCells.iterateCell(i,j),true);
-				}
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i==2&&j==1) {
+				assertTrue(panel.iterateCell(i,j));
+			}
+			else if (i==2&&i==2) {
+				assertTrue(panel.iterateCell(i,j));
+			}
+			else if (i==2&&i==3) {
+				assertTrue(panel.iterateCell(i,j));
 			}
 			else {
-				assertEquals(boardCells.iterateCell(i,j),false);
+				assertFalse(panel.iterateCell(i,j));
 			}
 		}
 	}
+	
 }
 @Test 
 	public void test3() {
